296b91c9465571582424d22cf6c634bf811cbe37
bbapp <hello@buildbox.app>
2020-11-08 18:42:47 +0300

    v1.0.2

--- test.go
+++ test.go
@@ -1,37 +1,37 @@
 package main
 
-import (
-	"fmt"
-	buildboxapp "github.com/buildboxapp/app/lib"
-)
-
-var app1 = buildboxapp.App{}
-
-func main()  {
-	params := []string{
-		"2020-01-02 15:04:05",
-		" ",
-		"-",
-		"-1",
-	}
-
-	//fmt.Println(app1.Time([]string{"THIS",""}))
-	result := app1.DReplace(params)
-
-	//arg := "@Time(now), '+10h' "
-	//
-	//args := strings.Split(arg, ",")
-	//// очищаем каждый параметр от ' если есть
-	//argsClear := []string{}
-	//for _, v := range args{
-	//	v = strings.Trim(v, " ")
-	//	v = strings.Trim(v, "'")
-	//	argsClear = append(argsClear, v)
-	//}
-	//
-	//for _, v := range argsClear {
-	//	fmt.Println("-"+v+"-")
-	//}
-
-	fmt.Println(result)
-}
+//import (
+//	"fmt"
+//	buildboxapp "github.com/buildboxapp/app/lib"
+//)
+//
+//var app1 = buildboxapp.App{}
+//
+//func main()  {
+//	params := []string{
+//		"2020-01-02 15:04:05",
+//		" ",
+//		"-",
+//		"-1",
+//	}
+//
+//	//fmt.Println(app1.Time([]string{"THIS",""}))
+//	result := app1.DReplace(params)
+//
+//	//arg := "@Time(now), '+10h' "
+//	//
+//	//args := strings.Split(arg, ",")
+//	//// очищаем каждый параметр от ' если есть
+//	//argsClear := []string{}
+//	//for _, v := range args{
+//	//	v = strings.Trim(v, " ")
+//	//	v = strings.Trim(v, "'")
+//	//	argsClear = append(argsClear, v)
+//	//}
+//	//
+//	//for _, v := range argsClear {
+//	//	fmt.Println("-"+v+"-")
+//	//}
+//
+//	fmt.Println(result)
+//}
